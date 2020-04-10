ee64bcb2c2ac683d5817c0182c154745527b0b0b
DoronGolan <doron.golan@ls-techs.com>
2020-04-10 17:37:33 +0300

    add cov name display

--- FrontEnd/talking-with-freds/src/Infrastructure/fetchers/MessagesFetcher.fetcher.ts
+++
@@ -1,5 +0,0 @@
-export default class MessagesFetcher {
-	public static async getConversationsMessages(convId: string) {
-		return [];
-	}
-}
--- FrontEnd/talking-with-freds/src/UI/components/ChatWindow/ChatTopBar/ChatTopBar.component.tsx
+++ FrontEnd/talking-with-freds/src/UI/components/ChatWindow/ChatTopBar/ChatTopBar.component.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ChatTopBar.component.scss';
 import {Image} from 'semantic-ui-react';
-import { isNullOrUndefined } from 'util';
+import {isNullOrUndefined} from 'util';
 
 interface IProps {
 	convName?: string;
@@ -15,11 +15,18 @@ export default class ChatTopBarComponent extends React.Component<IProps, IState>
 	public render() {
 		return (
 			<div className='top-bar-wrapper'>
-				<div className={`chat-img-wrapper ${!isNullOrUndefined(this.props.chatImage) && this.props.chatImage !== '' ? '' : 'default-image'}`}>
+				<div
+					className={`chat-img-wrapper ${
+						!isNullOrUndefined(this.props.chatImage) && this.props.chatImage !== '' ? '' : 'default-image'
+					}`}>
 					<div className='circular-image-wrapper'>
-						<Image className='chat-img' src={this.props.chatImage || require('../../../../static/images/blank_user_profile_image.jfif')} />
+						<Image
+							className='chat-img'
+							src={this.props.chatImage || require('../../../../static/images/blank_user_profile_image.jfif')}
+						/>
 					</div>
 				</div>
+				<div>{this.props.convName}</div>
 			</div>
 		);
 	}
