1a6dde58a01b638cfa5be2f91e48a843c7d5aa95
DoronGolan <doron.golan@ls-techs.com>
2020-04-14 05:36:49 +0300

    chat name change works

--- BackEnd/src/BL/Controllers/conversation.controller.js
+++ BackEnd/src/BL/Controllers/conversation.controller.js
@@ -11,29 +11,31 @@ class ConversationController {
 				.verifyToken(token)
 				.then(async (authData) => {
 					const conversations = await ConversationSchema.find({participants: authData.id});
-					return conversations.map(async (conv) => {
-						/**
-						 * for each conversation check if group
-						 * if not, change name and image to the
-						 * other user's username and profile image
-						 */
-						if (!conv.isGroup) {
-							if (conv.participants.length === 2) {
-								// find whom of the users are not
-								// the requesting user
-								const otherIndex = conv.participants[0] == authData.id ? 1 : 0;
-								// get user document
-								const user = await userController.getUserById(conv.participants[otherIndex]);
-								// set the conversation values.
-								conv.convName = user.username;
-								conv.profileImg = user.profileImage;
-								console.log(conv);
-								return conv;
+					return Promise.all(
+						conversations.map(async (conv) => {
+							/**
+							 * for each conversation check if group
+							 * if not, change name and image to the
+							 * other user's username and profile image
+							 */
+							if (!conv.isGroup) {
+								if (conv.participants.length === 2) {
+									// find whom of the users are not
+									// the requesting user
+									const otherIndex = conv.participants[0] == authData.id ? 1 : 0;
+									// get user document
+									const user = await userController.getUserById(conv.participants[otherIndex]);
+									// set the conversation values.
+									conv.convName = user.username;
+									conv.profileImg = user.profileImage;
+									console.log(conv);
+									return conv;
+								}
 							}
-						}
-						console.log(conv);
-						return conv;
-					});
+							console.log(conv);
+							return conv;
+						})
+					);
 				})
 				.catch((err) => {
 					throw new Error(err);
